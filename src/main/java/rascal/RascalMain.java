ea4af3514810abdb825444067d3ab57cc66f9d67
yaeldina <yaeldinaa@gmail.com>
2020-12-01 17:39:51 -0800

    fixed some problems

--- src/main/java/rascal/RascalMain.java
+++ src/main/java/rascal/RascalMain.java
@@ -1,26 +1,24 @@
 package rascal;
 
 import java.io.File;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class RascalMain {
-    public static void main(String[] args){
-        System.out.println(new File("TrianlgeInput.txt").getAbsolutePath());
-        File dataSet = new File("C:/Users/yaeldina/IdeaProjects/RascalsTriangle/TrianlgeInput.txt");
+    public static void main(String[] args) {
         RascalTriangle rascal = new RascalTriangle();
-        try{
-            Scanner scanner = new Scanner(dataSet, StandardCharsets.UTF_8.name());
-            System.out.println(scanner.nextLine());
-            while(scanner.hasNextLine()){
-                System.out.println(scanner.next());
-                rascal.R(scanner.nextInt(), scanner.nextInt());
+        try {
+            File file = new File("input");
+            Scanner scanner = new Scanner(file);
+            int P = Integer.parseInt(scanner.nextLine());
+            System.out.println(P);
+            for(int i = 0; i < P; i++){
+                if(scanner.hasNext()){
+                    System.out.print(scanner.next() + " ");
+                    System.out.println(rascal.R(scanner.nextInt(), scanner.nextInt()));
+                }
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
 }
--- src/main/java/rascal/RascalTriangle.java
+++ src/main/java/rascal/RascalTriangle.java
@@ -1,19 +1,17 @@
 package rascal;
 
-
 public class RascalTriangle {
-    private int item;
 
     public RascalTriangle() {
     }
 
-    public int R(int n, int m) {
+    public long R(int n, int m) {
+        long item;
         if (m == 0 || n == m) {
             item = 1;
         } else {
             item = (R(n - 1, m - 1) * R(n - 1, m) + 1) / R(n - 2, m - 1);
         }
-
         return item;
     }
 }
